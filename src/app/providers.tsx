98bb277fb2b6d5ad7075ed9b619155c00b66b311
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-09-15 15:11:33 +0300

    Added clerk authentication

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useAuth, ClerkProvider } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk client={client} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
+}
